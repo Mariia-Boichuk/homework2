b3ef13aa488c7dd5d12191237fda9414c9ebe2dd
mariia_boichuk <mariia_boichuk@epam.com>
2021-11-01 15:32:40 +0200

    2hw

---
+++ app.js
@@ -0,0 +1,82 @@
+const clients = document.getElementsByClassName(
+  "comments-container__client-name"
+);
+const cardsContainer = document.querySelector(".comments-container__cards");
+const textsCollection = document.getElementsByClassName(
+  "comments-container__text"
+);
+const jobsEls = document.getElementsByClassName(" comments-container__job");
+const avatarsEls = document.getElementsByClassName(" comments-container__ava");
+const loadButton = document.querySelector(".comments-container > button");
+const geoP = document.getElementById("adress-p");
+let shyrota;
+let dovgota;
+
+const fillElems = (elems, dataFromApi, prop) => {
+  for (let i = 0; i < 7; i++) {
+    elems[i].innerText = dataFromApi[i][prop];
+  }
+};
+
+loadButton.onclick = () => {
+  cardsContainer.classList.toggle("notvisible");
+  loadButton.classList.toggle("notvisible");
+};
+Promise.all([
+  fetch("https://random-data-api.com/api/users/random_user?size=7"),
+  fetch(
+    "https://random-data-api.com/api/lorem_ipsum/random_lorem_ipsum?size=7"
+  ),
+])
+  .then(function (responses) {
+    return Promise.all(
+      responses.map(function (response) {
+        return response.json();
+      })
+    );
+  })
+  .then(function (data) {
+    console.log(data[2]);
+    for (let i = 0; i < 7; i++) {
+      clients[i].innerText = `${data[0][i].first_name} ${data[0][i].last_name}`;
+
+      jobsEls[i].innerText = `${data[0][i].employment.title}`;
+
+      //  avatarsEls[i].src = `${data[0][i].avatar}`;
+    }
+    fillElems(textsCollection, data[1], "very_long_sentence");
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+//===========geo================
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+function success(pos) {
+  var crd = pos.coords;
+  shyrota = crd.latitude;
+  dovgota = crd.longitude;
+  console.log(shyrota, dovgota);
+  fetch(
+    `https://nominatim.openstreetmap.org/reverse?format=xml&lat=${shyrota}&lon=${dovgota}&zoom=18&addressdetails=1&format=json`
+  )
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((res) => {
+      console.log(res);
+      geoP.textContent = res.display_name;
+    });
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
